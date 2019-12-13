bd07220e2f873f53569bf2d98e11601aca8c3016
Jakob Rumpf <rumpf.jakob@gmail.com>
2019-12-13 12:28:49 +0100

    Einkommensteuerrechner

---
+++ 2AHME/ue_05_Einkommenssteuer/main.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+  char s[80];
+  double einkommen;
+  double steuer;
+ 
+  
+  printf("UE05 Einkommenessteuer\n");
+  printf("--------------------\n");
+  
+  fgets(s, 80, stdin);
+  sscanf(s,  "%lf", &einkommen);
+  if(einkommen < 0);{
+  printf("Eingabefehler\n");
+  return 1;
+}
+  
+  if(einkommen >= 1E6){
+    steuer= (einkommen - 1E6) * 0.55;
+    steuer = steuer + 10000 * 0.5;
+    steuer = steuer + 30000 * 0.48;
+    steuer = steuer + 29000 * 0.42;
+    
+  } else if (einkommen >= 90000){
+    
+    steuer = (einkommen - 90000) * 0.5;
+    steuer = steuer + 30000 * 0.48;
+    steuer = steuer + 29000 * 0.42;
+    
+  } else if (einkommen >= 60000 ){
+    
+    steuer = (einkommen - 60000) * 0.48;
+    steuer = steuer + 29000 * 0.42;
+    
+  } else if (einkommen >= 31000){
+   
+    steuer = (einkommen - 31000) * 0.42;
+    
+    
+  } else {
+    steuer = 0;
+  } 
+        
+      
+  
+  return 0;
+}
+
---
+++ 2AHME/ue_05_Einkommenssteuer/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=ue_05_einkommenssteuer
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/ue_05_einkommenssteuer
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=ue05einkommenssteuer.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/ue05einkommenssteuer.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=ue_05_einkommenssteuer
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/ue_05_einkommenssteuer
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=ue05einkommenssteuer.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/ue05einkommenssteuer.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
