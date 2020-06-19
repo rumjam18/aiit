70b65bc9ffca94e23958287104bcef9e6b09be7b
Jakob Rumpf <rumpf.jakob@gmail.com>
2020-06-19 12:42:29 +0200

    ue15_kleines_1x1

--- 2AHME/ue15_kleines-1x1/main.c
+++ 2AHME/ue15_kleines-1x1/main.c
@@ -1,33 +1,51 @@
 #include <stdio.h>
 
-void print2DimFeld(double f[10][10]) {
-    printf("print2DimFeld:\n");
-    for (int i = 1; i <=10; i++) {
-        for (int j = 1; j <= 10; j++) {
-            printf("feld[%d][%d] %p => %f\n",
-                   i, j, &f[i][j], f[i][j]);
-        }
-    }
-    printf("\n\n");
-}
-
-void zweidimensionalesFeld ();
-
-    double f[10][10] = { {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 
-                         {1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 
-                         {2, 2, 4, 6, 8, 10, 12, 14, 16 ,18, 20}, 
-                         {3, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30}, 
-                         {4, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40}, 
-                         {5, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50},
-                         {6, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60},
-                         {7, 7, 14, 21, 28, 35, 42, 49, 56, 63, 70}, 
-                         {8, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80},
-                         {9, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90},
-                         {10, 20, 27, 30, 40, 50, 60, 70, 80, 90, 100} };
- 
 int main () {
     
-    zweidimensionalesFeld();
+    int w[17][17];  
     
+    printf("ue15 Kleines_1x1:\n");
+    
+    for (int i = 0; i <= 16; i++) {
+        for (int j = 0; j <= 16; j++) {
+            w[i][j] = i * j;
+        }
+    }
+    
+    printf("\nIn Dezimal:\n\n");
+    printf("    |");
+    for (int i = 1; i <= 10; i++) {
+        printf("%4d |", i);
+    }
+    printf("\n");
+    printf("-----------------------------------------------------------------");
+    printf("\n");
+            
+    for (int i = 1; i <= 10; i++) {
+        printf("%3d |", i);
+        for (int j = 1; j <= 10; j++) {
+            printf("%4d |", w[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n\nIn Oktal:\n\n");
+    printf("    |");
+    for (int i = 1; i <= 8; i++) {
+        printf("%4o |", (unsigned)i);
+    }
+    printf("\n");
+    printf("-----------------------------------------------------");
+    printf("\n");
+            
+    for (int i = 1; i <= 8; i++) {
+        printf("%3o |", (unsigned)i);
+        for (int j = 1; j <= 8; j++) {
+            printf("%4o |", (unsigned)w[i][j]);
+        }
+        printf("\n");
+    }
+    
+
     return 0;
 }
