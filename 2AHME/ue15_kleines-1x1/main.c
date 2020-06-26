8dd2253ab2eb717455f491c38fd667cad2c3b75f
Jakob Rumpf <rumpf.jakob@gmail.com>
2020-06-26 12:11:43 +0200

    ue15_kleines-1x1

--- 2AHME/ue15_kleines-1x1/main.c
+++ 2AHME/ue15_kleines-1x1/main.c
@@ -2,12 +2,12 @@
 
 int main () {
     
-    int w[17][17];  
+    int w[16][16];  
     
     printf("ue15 Kleines_1x1:\n");
     
-    for (int i = 0; i <= 16; i++) {
-        for (int j = 0; j <= 16; j++) {
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
             w[i][j] = i * j;
         }
     }
@@ -43,9 +43,26 @@ int main () {
         for (int j = 1; j <= 8; j++) {
             printf("%4o |", (unsigned)w[i][j]);
         }
+        printf ("\n");
+    }
+        
+    printf("\n\nIm Hex-System:\n\n");
+    printf("    |");
+    for (int i = 1; i <= 15; i++) {
+        printf("%4x |", (unsigned)i);
+    }
+    printf("\n");
+    printf("-----------------------------------------------------------------------------------------------");
+    printf("\n");
+    
+    for (int i = 1; i <= 15; i++) {
+        printf("%3X |", (unsigned)i);
+        for (int j = 1; j <= 15; j++) {
+            printf("%4X |", (unsigned)w[i][j]);
+        }
         printf("\n");
     }
     
-
     return 0;
-}
+    }
+
